2d506ccf1b686dd646482ecf10bafa36aae067d4
pouya tirdad <pouyatirdad70@gmail.com>
2023-07-03 23:03:31 +0330

    model ,service ,controller add

---
+++ FormulaAirline.API/Controllers/BookingsController.cs
@@ -0,0 +1,17 @@
+﻿using FormulaAirline.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaAirline.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        private readonly IMessageProducer messageProducer;
+
+        public BookingsController(IMessageProducer messageProducer = null)
+        {
+            this.messageProducer = messageProducer;
+        }
+    }
+}
